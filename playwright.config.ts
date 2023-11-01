e9c61b071c9bfbd0c79543f10b93c75a2b1e92bd
jill64 <intents.turrets0h@icloud.com>
2023-11-01 10:54:07 +0900

    add test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   webServer: {
-    command: 'pnpm preview',
+    command: 'npm run preview',
     port: 4173
   },
   testDir: 'tests',
---
+++ tests/decimal.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@playwright/test'
+
+test('Decimal', async ({ page }) => {
+  await page.goto('/svelte-input')
+
+  const textarea = page.getByPlaceholder('Decimal Input')
+  await expect(textarea).toBeVisible()
+  await textarea.fill('42')
+  await expect(textarea).toHaveValue('42')
+  await page.getByRole('button', { name: 'ー' }).click()
+  await expect(textarea).toHaveValue('41')
+  await page.getByRole('button', { name: '＋', exact: true }).click()
+  await expect(textarea).toHaveValue('42')
+})
