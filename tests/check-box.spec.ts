686a35f36cf9d5503aec8dfac660d512ab882ebd
jill64 <intents.turrets0h@icloud.com>
2023-11-15 22:03:35 +0900

    update test

--- tests/check-box.spec.ts
+++ tests/check-box.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test('Check Box', async ({ page }) => {
   await page.goto('/')
 
-  const checkBox = page.getByLabel('Check Box')
+  const checkBox = page.getByLabel('Check Box', { exact: true })
   await expect(checkBox).toBeVisible()
   await checkBox.click()
   await expect(checkBox).toBeChecked()
