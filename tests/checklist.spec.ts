4a26635e47ad40deb80aa472bd4c72b76d81caca
jill64 <intents.turrets0h@icloud.com>
2023-11-20 18:50:56 +0900

    fix: test

--- tests/checklist.spec.ts
+++ tests/checklist.spec.ts
@@ -3,9 +3,9 @@ import { expect, test } from '@playwright/test'
 test('CheckList', async ({ page }) => {
   await page.goto('/')
 
-  const first = page.getByLabel('First', { exact: true })
-  const second = page.getByLabel('Second', { exact: true })
-  const third = page.getByLabel('Third', { exact: true })
+  const first = page.getByRole('checkbox', { name: 'First', exact: true })
+  const second = page.getByRole('checkbox', { name: 'Second', exact: true })
+  const third = page.getByRole('checkbox', { name: 'Third', exact: true })
 
   await expect(first).toBeVisible()
   await expect(second).toBeVisible()
@@ -23,13 +23,13 @@ test('CheckList', async ({ page }) => {
 
   await second.click()
 
-  await expect(first).not.toBeChecked()
+  await expect(first).toBeChecked()
   await expect(second).toBeChecked()
   await expect(third).not.toBeChecked()
 
   await third.click()
 
-  await expect(first).not.toBeChecked()
-  await expect(second).not.toBeChecked()
+  await expect(first).toBeChecked()
+  await expect(second).toBeChecked()
   await expect(third).toBeChecked()
 })
