4b898e529519379924947ee264aac8c89bfa61dc
jill64 <intents.turrets0h@icloud.com>
2023-11-20 18:00:05 +0900

    fix: test

--- tests/textarea.spec.ts
+++ tests/textarea.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test('TextArea', async ({ page }) => {
   await page.goto('/')
 
-  const textarea = page.getByPlaceholder('Auto Grow Text Area')
+  const textarea = page.getByPlaceholder('Auto Growing Text Area')
   await expect(textarea).toBeVisible()
   await textarea.fill('Hello World')
   await expect(textarea).toHaveValue('Hello World')
