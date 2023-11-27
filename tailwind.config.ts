30f966194521e7b1122794e9a8071807a5fd5a52
jill64 <intents.turrets0h@icloud.com>
2023-11-27 17:30:47 +0900

    add tailwind for demo

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
+
+export default config
